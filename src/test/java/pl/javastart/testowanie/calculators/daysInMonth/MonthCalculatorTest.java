7b104fdfde8545adc3c9af8405e4c7dff8887c7b
JakubMieczkowski(109 <jakm5000@wp.pl>
2023-02-12 21:06:29 +0100

    ParameterizedTest

--- src/test/java/pl/javastart/testowanie/calculators/daysInMonth/MonthCalculatorTest.java
+++ src/test/java/pl/javastart/testowanie/calculators/daysInMonth/MonthCalculatorTest.java
@@ -4,14 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import pl.javastart.testowanie.calculators.daysInMonth.MonthCalculator;
 
 
 public class MonthCalculatorTest {
 
     private MonthCalculator monthCalculator;
+
     @BeforeEach
-    public void init(){
+    public void init() {
         monthCalculator = new MonthCalculator();
     }
 
@@ -25,6 +29,7 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(30, dayCount);
     }
+
     @DisplayName("Days in 07.2018")
     @Test
     public void shouldReturn31For2018_07() {
@@ -34,6 +39,7 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(31, dayCount);
     }
+
     @DisplayName("Days in 08.2018")
     @Test
     public void shouldReturn31For2018_08() {
@@ -43,6 +49,7 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(31, dayCount);
     }
+
     @DisplayName("Days in 02.2018")
     @Test
     public void shouldReturn28For2018_02() {
@@ -62,6 +69,7 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(29, dayCount);
     }
+
     @DisplayName("Days in 02.2010")
     @Test
     public void shouldReturn28For2010_02() {
@@ -71,6 +79,7 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(28, dayCount);
     }
+
     @DisplayName("Days in 02.2000")
     @Test
     public void shouldReturn28For2000_02() {
@@ -80,11 +89,56 @@ public class MonthCalculatorTest {
         // then
         Assertions.assertEquals(29, dayCount);
     }
+
     @DisplayName("Days in 02.2100")
     @Test
     public void shouldReturn28For2100_02() {
         // when
         int dayCount = monthCalculator.getDayCount(2100, 2);
+
+        // then
+        Assertions.assertEquals(28, dayCount);
+    }
+
+    @DisplayName("Days of the month for the year")
+    @ParameterizedTest(name = "Year {arguments} month has 31 days")
+    @CsvSource({"2018,07", "2018,08"})
+    public void shouldReturn31ForMonthInYear(String year, String month) {
+        // when
+        int dayCount = monthCalculator.getDayCount(Integer.parseInt(year), Integer.parseInt(month));
+
+        // then
+        Assertions.assertEquals(31, dayCount);
+    }
+
+    @DisplayName("Days of the month for the year")
+    @ParameterizedTest(name = "Year {arguments} month has 30 days")
+    @CsvSource("2018,06")
+    public void shouldReturn30ForMonthInYear(String year, String month) {
+        // when
+        int dayCount = monthCalculator.getDayCount(Integer.parseInt(year), Integer.parseInt(month));
+
+        // then
+        Assertions.assertEquals(30, dayCount);
+    }
+
+    @DisplayName("Days of the month for the year")
+    @ParameterizedTest(name = "Year {arguments} month has 29 days")
+    @CsvSource({"2016,02", "2000,02"})
+    public void shouldReturn29ForMonthInYear(String year, String month) {
+        // when
+        int dayCount = monthCalculator.getDayCount(Integer.parseInt(year), Integer.parseInt(month));
+
+        // then
+        Assertions.assertEquals(29, dayCount);
+    }
+
+    @DisplayName("Days of the month for the year")
+    @ParameterizedTest(name = "Year {arguments} month has 28 days" )
+    @CsvSource({"2018,02", "2010,02","2100,02"})
+    public void shouldReturn28ForMonthInYear(String year,String month) {
+        // when
+        int dayCount = monthCalculator.getDayCount(Integer.parseInt(year), Integer.parseInt(month));
 
         // then
         Assertions.assertEquals(28, dayCount);
